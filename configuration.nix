82b18742862ca2459b9aa85bf8a7e1f22beb8e4f
Toby Selway <tobyselway@outlook.com>
2023-05-04 01:44:28 +0100

    DNS fix

--- configuration.nix
+++ configuration.nix
@@ -24,6 +24,11 @@
   networking.hostName = "studio"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  networking.nameservers = [
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
